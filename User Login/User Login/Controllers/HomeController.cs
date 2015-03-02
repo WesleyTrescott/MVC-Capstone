b03cc5d37fba11ac77674ba4cf0661094ac4b1e9
WesleyTrescott <wesley.trescott@gmail.com>
2015-03-01 20:46:55 -0500

    Updated featured jobs on home page

--- User Login/User Login/Controllers/HomeController.cs
+++ User Login/User Login/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using User_Login.Models;
 
 namespace User_Login.Controllers
 {
@@ -12,7 +13,11 @@ namespace User_Login.Controllers
         // GET: /Home/
         public ActionResult Index()
         {
-            return View();
+            //var entities = new EnhancedJobCandidateAppEntities();
+            var entities = new Job_Candidate_Application_Entities();
+            IEnumerable<Tbl_Jobs> mylist = entities.Tbl_Jobs.ToList();
+            var sixRandomFoos = mylist.OrderBy(x => Guid.NewGuid()).Take(6);
+            return View(sixRandomFoos);
         }
 	}
 }
