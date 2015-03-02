d05b62ae224b61d3f76c5553d02b770cc62d757a
WesleyTrescott <wesley.trescott@gmail.com>
2015-03-01 22:59:34 -0500

    Added featured jobs functionality on home page

--- User Login/User Login/Controllers/HomeController.cs
+++ User Login/User Login/Controllers/HomeController.cs
@@ -13,7 +13,6 @@ namespace User_Login.Controllers
         // GET: /Home/
         public ActionResult Index()
         {
-            //var entities = new EnhancedJobCandidateAppEntities();
             var entities = new Job_Candidate_Application_Entities();
             IEnumerable<Tbl_Jobs> mylist = entities.Tbl_Jobs.ToList();
             var sixRandomFoos = mylist.OrderBy(x => Guid.NewGuid()).Take(6);
